2852d7c854ce7a40a08fd4538f60dd6bde0ca1d8
Steve Ferreira <sjferreira516@gmail.com>
2021-07-08 10:19:23 -0400

    Create basic client app structure and connect to posts api

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
 app.get('/posts', (req, res) => {
+    console.log('Request > GET /posts...');
     res.send(posts);
 });
 
 app.post('/posts', (req, res) => {
+    console.log('Request > POST /posts...');
     const { title } = req.body
     const id = randomBytes(4).toString('hex');
 
