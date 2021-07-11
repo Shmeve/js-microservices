615966b3d8a60a8444e2383cb62f95cf4dbce9d7
Steve Ferreira <sjferreira516@gmail.com>
2021-07-11 12:35:26 -0400

    Connect comments service to event bus and publish comment events

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const { content } = req.body
     const postId = req.params.id;
     const commentId = randomBytes(4).toString('hex');
@@ -24,7 +25,22 @@ app.post('/posts/:id/comments', (req, res) => {
 
     commentsByPostId[postId] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId
+        }
+    });
+
     res.status(201).send(comments);
+});
+
+app.post('/events', async (req, res) => {
+    console.log('Received Event >', req.body);
+
+    res.send({});
 });
 
 app.listen(4001, () => {
