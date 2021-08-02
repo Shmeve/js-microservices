a06de8ecbbc0f2c84bbdf31244083160c19ea367
Steve Ferreira <sjferreira516@gmail.com>
2021-08-02 15:28:32 -0400

    Add ingress service and serve auth

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -6,6 +6,10 @@ const PORT = 3000;
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 })
