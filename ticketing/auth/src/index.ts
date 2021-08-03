fb70a31bb0eabd65d8db5f2a285ae389ff48b700
Steve Ferreira <sjferreira516@gmail.com>
2021-08-03 09:10:35 -0400

    Setup router boilerplate for auth service

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,14 +1,23 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+// Routers
+import { currentUserRouter } from './routes/current-user';
+import { signinrRouter } from './routes/signin';
+import { signoutrRouter } from './routes/signout';
+import { signuprRouter} from './routes/signup';
+
 const PORT = 3000;
 
+// Express
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hello World!');
-});
+// Connect Routers
+app.use(currentUserRouter);
+app.use(signinrRouter);
+app.use(signoutrRouter);
+app.use(signuprRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
